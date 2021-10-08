236ea6ad2e59536ebafc82e2c7631714021398c6
Ognen Kirov Todorovski <35491291+Ognen67@users.noreply.github.com>
2021-10-09 01:23:45 +0200

    Bullet scripts from Evgenij

---
+++ GameJam2021Oct/Assets/Shooting.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+    public float shootCoolDown = 2f;
+    float nextShootTime = 0f;
+    float shootTime;
+    public float startShootTime;
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    void Start()
+    {
+        shootTime = startShootTime;
+    }
+    void Update()
+    {
+        Shoot();
+       
+    }
+    public void Shoot()
+    {
+        if (Time.time > nextShootTime)
+        {
+          
+                if (shootTime <= 0)
+                {
+                    shootTime = startShootTime;
+                   
+                }
+                else
+                {
+                    shootTime -= Time.deltaTime;
+                    Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+                }
+                nextShootTime = Time.time + shootCoolDown;
+            }
+        
+    }
+
+}

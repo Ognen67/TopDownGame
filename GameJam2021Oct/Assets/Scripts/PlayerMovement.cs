2f73caca5a2284747796c659164bbccb544777ac
Ognen Kirov Todorovski <35491291+Ognen67@users.noreply.github.com>
2021-10-09 02:03:17 +0200

    PickUp functionallity finished

--- GameJam2021Oct/Assets/Scripts/PlayerMovement.cs
+++ GameJam2021Oct/Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    // Basic Movement
     public float moveSpeed = 5f;
-
     public Rigidbody2D rb;
-
     Vector2 movement;
 
-    // Update is called once per frame
+    // Pick Up
+    private PickUp pickUp;
+
+    private void Start()
+    {
+        pickUp = gameObject.GetComponent<PickUp>();
+        pickUp.Direction = new Vector2(0, 1);
+    }
+
+
     void Update()
     {
+        
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if(movement.sqrMagnitude > .1f)
+        {
+            pickUp.Direction = movement.normalized;
+        }
     }
 
     void FixedUpdate()

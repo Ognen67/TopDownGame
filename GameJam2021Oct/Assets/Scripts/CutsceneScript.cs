37124a120e9ed0ea51be94657ac50a3ac4f60b14
JanStankov <jan.stankov@gmail.com>
2021-10-09 13:01:48 +0200

    menu / cutscene / doors

---
+++ GameJam2021Oct/Assets/Scripts/CutsceneScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class CutsceneScript : MonoBehaviour
+{
+    public GameObject text1;
+    public GameObject text2;
+    public GameObject text3;
+    public GameObject text4;
+    //public GameObject text5;
+    public float sec = 5f;
+    public string NewLevel = "Level1";
+    void Start()
+    {
+        text1.SetActive(false);
+        text2.SetActive(false);
+        text3.SetActive(false);
+        text4.SetActive(false);
+        //text5.SetActive(false);
+        StartCoroutine(LoadLevelAfterDelay());
+        
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            SceneManager.LoadScene(NewLevel);
+        }
+    }
+    IEnumerator LoadLevelAfterDelay()
+    {
+        text1.SetActive(true);
+        yield return new WaitForSeconds(sec);
+        text1.SetActive(false);
+        text2.SetActive(true);
+        yield return new WaitForSeconds(sec);
+        text2.SetActive(false);
+        text3.SetActive(true);
+        yield return new WaitForSeconds(sec);
+        text3.SetActive(false);
+        text4.SetActive(true);
+        yield return new WaitForSeconds(sec);
+        text4.SetActive(false);
+        //text5.SetActive(true);
+        //yield return new WaitForSeconds(sec);
+        SceneManager.LoadScene(NewLevel);
+    }
+
+
+}
---
+++ GameJam2021Oct/Assets/Scripts/PresurePlate.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresurePlate : MonoBehaviour
+{
+    [SerializeField] private GameObject door1GameObject;
+    [SerializeField] private GameObject door2GameObject;
+    private DoorTrigger door1;
+    private DoorTrigger door2;
+    float timer;
+    public float TimeOfTimer;
+
+    private void Awake()
+    {
+        door1 = door1GameObject.GetComponent<DoorTrigger>();
+        door2 = door2GameObject.GetComponent<DoorTrigger>();
+
+    }
+    private void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0f) {
+                door1.CloseDoor();
+                door2.CloseDoor();
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       
+        if (other.gameObject.tag == "Player") {
+            door1.OpenDoor();
+            door2.OpenDoor();
+            
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        
+        if (other.gameObject.tag == "Player")
+        {
+            timer = TimeOfTimer;
+        }
+    }
+}
